2d548c8362dd27faed2d4faa0abe3a2428020954
EStherMmutisya <starjacob994@gmail.com>
2023-12-12 11:27:55 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let multiply= num1 * num2 
+let num1 = 31 
+let num2 = 2 
+multiply = 31 * 2
+
+let random = 64 
+
+let mod = num3 / num4
+let num3 = 9
+let num4 = 5
+
+mod = num3 % num4 
+
+let max = 20 
